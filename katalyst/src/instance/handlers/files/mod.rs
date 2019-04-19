4b874daa070bf638422b6a4c72f0494030351a8f
Phil Proctor <philliptproctor@gmail.com>
2019-04-19 17:09:53 -0400

    File server correctly sets the Content-Type based on the guessed content type to associate with the file extension

--- katalyst/src/instance/handlers/files/mod.rs
+++ katalyst/src/instance/handlers/files/mod.rs
@@ -2,6 +2,7 @@ use super::*;
 use crate::expression::*;
 use crate::prelude::*;
 use futures::future::*;
+use http::header::HeaderValue;
 use hyper::{Body, Response};
 use std::path::PathBuf;
 
@@ -17,21 +18,35 @@ impl Dispatchable for FileServer {
         if let Ok(path) = path {
             let mut full_path = PathBuf::from(&self.root_path);
             full_path.push(&path);
-            send_file(ctx, full_path.to_str().unwrap_or_default().to_string())
+            send_file(ctx, full_path)
         } else {
             Box::new(err::<Context, RequestFailure>(RequestFailure::Internal))
         }
     }
 }
 
-fn send_file(mut ctx: Context, filename: String) -> AsyncPipelineResult {
-    let result = Box::new(tokio_fs::file::File::open(filename).and_then(|file| {
-        let buf: Vec<u8> = Vec::new();
-        tokio_io::io::read_to_end(file, buf)
-            .and_then(|item| Ok(Response::<Body>::new(item.1.into())))
-    }));
-    Box::new(result.then(|result| match result {
-        Ok(r) => {
+fn send_file(mut ctx: Context, file: PathBuf) -> AsyncPipelineResult {
+    let result = Box::new(
+        tokio_fs::file::File::open(file.to_str().unwrap_or_default().to_string()).and_then(
+            |file| {
+                let buf: Vec<u8> = Vec::new();
+                tokio_io::io::read_to_end(file, buf)
+                    .and_then(|item| Ok(Response::<Body>::new(item.1.into())))
+            },
+        ),
+    );
+    Box::new(result.then(move |result| match result {
+        Ok(mut r) => {
+            let mime = mime_guess::get_mime_type_str(
+                file.extension()
+                    .unwrap_or_default()
+                    .to_str()
+                    .unwrap_or_default(),
+            )
+            .unwrap_or("application/octet-stream");
+            let hdrs = r.headers_mut();
+            let hdr_val = HeaderValue::from_str(mime).unwrap();
+            hdrs.append("Content-Type", hdr_val);
             ctx.upstream.response = Some(r);
             ok::<Context, RequestFailure>(ctx)
         }
