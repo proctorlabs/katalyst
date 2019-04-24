dc8feb941668d8fa71da0559a4e686564fab0482
Phil Proctor <philliptproctor@gmail.com>
2019-04-24 17:34:50 -0400

    Fixed JSON bindings and other minor changes

--- katalyst/src/context/data.rs
+++ katalyst/src/context/data.rs
@@ -2,25 +2,39 @@ use std::any::{Any, TypeId};
 use std::collections::HashMap;
 use std::sync::Arc;
 
+#[derive(Debug)]
+struct Container<T: Any + Send + Sync> {
+    data: Arc<T>,
+}
+
+impl<T: Any + Send + Sync> Container<T> {
+    fn new(obj: T) -> Self {
+        Container {
+            data: Arc::new(obj),
+        }
+    }
+
+    fn get(&self) -> Arc<T> {
+        self.data.clone()
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct ContextData {
-    store: HashMap<TypeId, Arc<Any + Send + Sync>>,
+    store: HashMap<TypeId, Box<Any + Send + Sync>>,
 }
 
 impl ContextData {
     pub fn get<T: Any + Send + Sync>(&self) -> Option<Arc<T>> {
         let id = TypeId::of::<T>();
-        println!("id: {:?}", id);
         let result = self.store.get(&id)?;
-        println!("retrieved");
-        let dc = result.downcast_ref::<Arc<T>>()?;
-        println!("casted");
-        Some(dc.clone())
+        let dc = result.downcast_ref::<Container<T>>()?;
+        Some(dc.get())
     }
 
     pub fn set<T: Any + Send + Sync>(&mut self, item: T) {
         let id = TypeId::of::<T>();
         println!("id: {:?}", id);
-        self.store.insert(id, Arc::new(item));
+        self.store.insert(id, Box::new(Container::new(item)));
     }
 }
--- katalyst/src/expression/bindings/json.rs
+++ katalyst/src/expression/bindings/json.rs
@@ -7,11 +7,8 @@ binding! {
         #[args(count=1)]
         fn value(ctx: &Context, args: &[ExpressionArg]) -> ExpressionResult {
             let key = args[0].render(ctx)?;
-            println!("{}", key);
             let json: Arc<serde_json::Value> = ctx.get_extension_data()?;
-            println!("{}", json);
             let res = &json[&key];
-            println!("{}", res);
             Ok(res.as_str().ok_or_else(|| RequestFailure::Internal)?.to_string())
         };
     }
